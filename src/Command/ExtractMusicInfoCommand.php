306abe4ce9331bd68ad1ba1fc5135419f0637b73
Christian Fecteau <christian.fecteau@gmail.com>
2018-01-27 16:52:05 -0500

    progress bar

--- src/Command/ExtractMusicInfoCommand.php
+++ src/Command/ExtractMusicInfoCommand.php
@@ -6,6 +6,7 @@ namespace Chrif\SimonRackham\Command;
 
 use Chrif\SimonRackham\MusicPageParser;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -21,8 +22,16 @@ class ExtractMusicInfoCommand extends Command {
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$parser = new MusicPageParser();
 
+		$output->writeln('Result will be written to parsed.csv');
+
+		$progress = new ProgressBar($output, 1);
+		$progress->setFormat('verbose');
+		$progress->start();
+
 		$result = $parser->parse(__DIR__ . '/../../resource/music-index.html', __DIR__ . '/../../parsed.csv');
 
+		$progress->finish();
+		$output->writeln('');
 		$output->writeln($result);
 
 		return 0;
--- src/Command/UpdateMusicPageCommand.php
+++ src/Command/UpdateMusicPageCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Chrif\SimonRackham\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -18,12 +19,45 @@ class UpdateMusicPageCommand extends Command {
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$html = file_get_contents('http://www.simonrackhamswork.com/music/');
+		$output->writeln('Updating music page');
+
+		/** @var ProgressBar $progress */
+		$progress = new ProgressBar($output, 100);
+		$progress->setFormat('verbose');
+		$progress->start();
+
+		$context = stream_context_create([], ['notification' => $this->notifier($progress)]);
+
+		$html = file_get_contents('http://www.simonrackhamswork.com/music/', false, $context);
+
+		$progress->finish();
 
 		file_put_contents(__DIR__ . '/../../resource/music-index.html', $html);
 
+		$output->writeln('');
 		$output->writeln('Music page updated');
 
 		return 0;
 	}
+
+	private function notifier(ProgressBar $progressBar) {
+		return (function (
+			$notification_code,
+			$severity,
+			$message,
+			$message_code,
+			$bytes_transferred,
+			$bytes_max
+		) use ($progressBar) {
+			switch ($notification_code) {
+				case STREAM_NOTIFY_FILE_SIZE_IS:
+					$progressBar->start($bytes_max);
+					break;
+				case STREAM_NOTIFY_PROGRESS:
+					$progressBar->setProgress($bytes_transferred);
+					break;
+			}
+		})->bindTo($this);
+	}
+
 }
